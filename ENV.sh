c4a74df6324c99d299616c5911d8f5765c5f3c21
azaa1 <azaa.chi11@gmail.com>
2019-03-09 16:48:53 +0000

    ENV.sh added

---
+++ ENV.sh
@@ -0,0 +1,4 @@
+export NODE_SIZE=${NODE_SIZE:-t2.micro}
+export MASTER_SIZE=${MASTER_SIZE:-t2.micro}
+export ZONES=${ZONES:-"eu-west-1a,eu-west-2b,eu-west-1c"}
+export KOPS_STATE_STORE="s3://myterraform.com"
